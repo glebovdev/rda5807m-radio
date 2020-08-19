40f3b0a8562ae0155316737c7c51d112c143c255
glebovdev <hi@ilyaglebov.dev>
2020-08-19 23:25:44 +0300

    Add SSD1306Ascii library with initial OLED setup

--- rda5807m/rda5807m.ino
+++ rda5807m/rda5807m.ino
@@ -1,13 +1,22 @@
+
+
 #include <radio.h>
 #include <RDA5807M.h>
+#include <Wire.h>
+#include <SSD1306Ascii.h>
+#include <SSD1306AsciiWire.h>
+
 
 // Initial radio settings
-
 #define RDA_BAND     RADIO_BAND_FM
 #define RDA_STATION  10530 
 #define RDA_VOLUME   1
 
+#define OLED_I2C_ADDRESS 0x3C
+//#define RST_PIN -1
+
 RDA5807M radio;
+SSD1306AsciiWire oled;
 
 void setup() {
   radio.init();
@@ -15,6 +24,18 @@ void setup() {
   radio.setVolume(RDA_VOLUME);
   radio.setMono(false);
   radio.setMute(false);
+
+  Wire.begin();
+  Wire.setClock(400000L);
+  oled.begin(&SH1106_128x64, OLED_I2C_ADDRESS);
+  oled.setFont(System5x7);
+  
+  oled.clear();
+  
+  oled.println();
+  oled.set2X();
+  oled.println("105.3 FM");
+
 }
 
 void loop() {
